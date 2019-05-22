e56a08b0565ec19747e0a0b9737548f06d9ea50c
Sorin Chis <kish.sorin@yahoo.com>
2019-05-22 13:24:08 +0300

    added show  items by category functionality

--- app-ecommerce/src/components/Phones/Phones.jsx
+++ app-ecommerce/src/components/Phones/Phones.jsx
@@ -1,73 +1,99 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchPhones, loadMorePhones, addPhoneToBasket, searchPhone, fetchCategories } from '../../store/actions/index';
-import { Link } from 'react-router-dom';
-import './Phones.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  fetchPhones,
+  loadMorePhones,
+  addPhoneToBasket,
+  searchPhone,
+  fetchCategories
+} from "../../store/actions/index";
+import { Link } from "react-router-dom";
+import "./Phones.css";
 
 class Phones extends Component {
-	componentDidMount() {
-		this.props.fetchPhones();
-		this.props.searchPhone();
-		this.props.fetchCategories();
-	}
+  componentDidMount() {
+    this.props.fetchPhones();
+    this.props.searchPhone();
+    this.props.fetchCategories();
+  }
 
-	renderPhone(phone, index) {
-		const shortDescription = phone.description;
-		const { addPhoneToBasket } = this.props;
-		return (
-			<div className="phone-card" key={index}>
-				<div className="thumbnail">
-					<img src={phone.image} alt={phone.name} />
-				</div>
-				<div>
-					<div className="name-price">
-						<h4>
-							<Link to={`/phone/${phone.id}`}>{phone.name}</Link>
-						</h4>
-						<h4>price: {phone.price} $ </h4>
-					</div>
+  renderPhone(phone, index) {
+    const shortDescription = phone.description;
+    const { addPhoneToBasket } = this.props;
 
-					<p className="description">{shortDescription}</p>
-					<div className="btn-more">
-						<button onClick={() => addPhoneToBasket(phone.id, phone.price, phone)}>Buy Now</button>
-						<Link to={`/phone/${phone.id}`}>More Info</Link>
-					</div>
-				</div>
-			</div>
-		);
-	}
+    //console.log('dataaaa ',this.props.phonesData);
+    return (
+      <div className="phone-card" key={index}>
+        <div className="thumbnail">
+          <img src={phone.image} alt={phone.name} />
+        </div>
+        <div>
+          <div className="name-price">
+            <h4>
+              <Link to={`/phone/${phone.id}`}>{phone.name}</Link>
+            </h4>
+            <h4>price: {phone.price} $ </h4>
+          </div>
 
-	render() {
-		//const { phones } = this.props;
-		return (
-			<div>
-				<div className="cards">
-					{this.props.searchResult.length ? (
-						this.props.searchResult.map((phone) => this.renderPhone(phone))
-					) : (
-						this.props.phonesData.map((phone, idx) => this.renderPhone(phone, idx))
-					)}
-				</div>
-				<div className="load-more">
-					<button onClick={this.props.loadMorePhones}>Load More Products</button>
-				</div>
-			</div>
-		);
-	}
+          <p className="description">{shortDescription}</p>
+          <div className="btn-more">
+            <button
+              onClick={() => addPhoneToBasket(phone.id, phone.price, phone)}
+            >
+              Buy Now
+            </button>
+            <Link to={`/phone/${phone.id}`}>More Info</Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    console.log("phone Data: ", this.props.categoryPressed);
+    console.log("phone Data: ", this.props.categoriesData);
+    return (
+      <div>
+        <div className="cards">
+          
+          {
+            this.props.categoryPressed ? this.props.categoriesData.map(phone => this.renderPhone(phone)) :
+
+            this.props.searchResult.length
+            ? this.props.searchResult.map(phone => this.renderPhone(phone))
+            : this.props.phonesData.map((phone, idx) =>
+                this.renderPhone(phone, idx)
+              )
+              
+          }
+        </div>
+        <div className="load-more">
+          <button onClick={this.props.loadMorePhones}>
+            Load More Products
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-	return {
-		phonesData: state.phones.phones,
-		searchResult: state.phones.searchResult
-	};
+const mapStateToProps = state => {
+  return {
+    phonesData: state.phones.phones,
+    searchResult: state.phones.searchResult,
+    categoriesData: state.phones.categoriesData,
+    categoryPressed: state.phones.categoryPressed
+  };
 };
 const mapDispatchToProps = {
-	fetchPhones,
-	loadMorePhones,
-	addPhoneToBasket,
-	searchPhone,
-	fetchCategories
+  fetchPhones,
+  loadMorePhones,
+  addPhoneToBasket,
+  searchPhone,
+  fetchCategories
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Phones);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Phones);
